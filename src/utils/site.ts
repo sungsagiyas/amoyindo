ace8a8194ac86c70f9fb83aebfa471828c4d6d63
sungsagiyas <sungsagiyas@gmail.com>
2025-07-15 14:19:50 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Amoy Indo';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Amoy Viral Colmek Sma Cindo Tante China';
-export const url = 'https://amoyindo.pages.dev';
-export const terbit = '2025-01-27T00:01:03Z';
